fbf4f7806331d40f09899e98b9a925e49013ebde
CylonicRaider <CylonicRaider@users.noreply.github.com>
2022-01-10 23:58:27 +0100

    Add setuptools boilerplate

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: ascii -*-
 
-from flask import Flask
-
-app = Flask('prpn')
-
-@app.route('/')
-def handle_root():
-    return ('Hello World!', {'Content-Type': 'text/plain; charset=utf-8'})
+from prpn import app
 
 if __name__ == '__main__': app.run()
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: ascii -*-
+
+from setuptools import setup
+
+setup(
+    name='prpn',
+    version='0.1',
+    packages=['prpn'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['flask']
+)
