2333626e0c3996c85a4096ea9d344c3f5489a108
CylonicRaider <CylonicRaider@users.noreply.github.com>
2022-01-17 18:10:45 +0100

    Factor out basic page template

--- prpn/main.py
+++ prpn/main.py
@@ -9,6 +9,8 @@ from . import db
 
 app = flask.Flask('prpn')
 
+app.jinja_options = {'trim_blocks': True, 'lstrip_blocks': True}
+
 _database = db.LockedDatabase(os.environ.get('DATABASE',
     os.path.join(app.root_path, '..', 'db.sqlite')))
 get_db = _database.register_to(app, flask.g)
