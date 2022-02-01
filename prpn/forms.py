a195ec8348c67a6153892c9c39eae62ef69e28af
CylonicRaider <CylonicRaider@users.noreply.github.com>
2022-02-01 15:17:55 +0100

    Rework form renderer

--- prpn/forms.py
+++ prpn/forms.py
@@ -4,49 +4,72 @@ from markupsafe import Markup, escape
 DEFAULT_METHOD = 'post'
 DEFAULT_ENCTYPE = 'application/x-www-form-urlencoded'
 
-def render_form(title, action, fields, method=None, enctype=None):
+def render_form(title, action, fields, method=None, enctype=Ellipsis):
+    def maybe_attr(name, value):
+        return Markup(' %s="%s"' % (name, value)) if value is not None else ''
+
     if method is None: method = DEFAULT_METHOD
-    if enctype is None: enctype = DEFAULT_ENCTYPE
-    result = [Markup('<form action="%s" method="%s" enctype="%s" '
-                           'class="mini-form mx-auto">\n') %
-                  (action, method, enctype)]
+    if enctype is Ellipsis: enctype = DEFAULT_ENCTYPE
+
+    result = [Markup('<form action="%s" method="%s"%s '
+                           'class="mini-form mx-auto">') %
+                  (action, method, maybe_attr('enctype', enctype))]
+
     if title is not None:
-        result.append(Markup('  <h2>%s</h2>\n') % (title,) if title else '')
+        result.append(Markup('  <h2>%s</h2>') % (title,) if title else '')
+
     for record in fields:
         if not record: continue
         name, ftype, label = record[:3]
-        value = None if len(record) == 3 else record[3]
+        if name is None or len(record) == 3 or record[3] is None:
+            value, value_attrs = None, ''
+        else:
+            value = record[3]
+            value_attrs = Markup(' name="%s" value="%s"') % (name, value)
+
         if ftype in ('submit', 'reset', 'button'):
             result.extend((
-                Markup('  <div class="d-grid">\n'),
-                Markup('    <input type="%s" id="%s" value="%s" '
-                                  'class="btn btn-primary"/>\n') %
-                    (ftype, name, label),
-                Markup('  </div>\n')
+                Markup('  <div class="d-grid">'),
+                Markup('    <button type="%s"%s '
+                                   'class="btn btn-primary">%s</button>') %
+                    (ftype, value_attrs, label),
+                Markup('  </div>')
             ))
+
         elif ftype in ('checkbox', 'radio'):
             result.extend((
-                Markup('  <div class="form-check mb-3">\n'),
-                Markup('    <input type="checkbox" id="%s" name="%s" ') %
-                    (name, name),
-                ('' if value is None else Markup('value="%s" ') % (value,)),
-                Markup('class="form-check-input"/>\n'),
-                Markup('  <label for="%s" '
-                                'class="form-check-label">%s</label>') %
+                Markup('  <div class="form-check mb-3">'),
+                Markup('    <input type="%s" id="%s"%s '
+                                  'class="form-check-input"/>') %
+                    (ftype, name, value_attrs),
+                Markup('    <label for="%s" '
+                                  'class="form-check-label">%s</label>') %
                     (name, label),
-                Markup('  </div>\n')
+                Markup('  </div>')
             ))
+
+        elif ftype == 'label':
+            result.append(
+                Markup('  <p class="mb-3"%s>%s</p>') %
+                    (maybe_attr('id', name), label)
+            )
+
+        elif ftype == 'hidden':
+            result.append(
+                Markup('  <input type="hidden"%s%s/>') %
+                    (maybe_attr('id', name), value_attrs)
+            )
+
         else:
             result.extend((
-                Markup('  <div class="mb-3">\n'),
-                Markup('    <label for="%s" '
-                                  'class="form-label">%s</label>\n') %
+                Markup('  <div class="mb-3">'),
+                Markup('    <label for="%s" class="form-label">%s</label>') %
                     (name, label),
-                Markup('    <input type="%s" id="%s" name="%s" ') %
-                    (ftype, name, name),
-                ('' if value is None else Markup('value="%s" ') % (value,)),
-                Markup('class="form-control"/>\n'),
-                Markup('  </div>\n')
+                Markup('    <input type="%s" id="%s" name="%s"%s '
+                                  'class="form-control"/>') %
+                    (ftype, name, name, maybe_attr('value', value)),
+                Markup('  </div>')
             ))
-    result.append(Markup('</form>\n'))
-    return Markup('').join(result)
+
+    result.extend((Markup('</form>'), ''))
+    return Markup('\n').join(result)
