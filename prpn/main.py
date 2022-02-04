020e0862c0bfd0b6a3f5001d9a76b1ce5276449b
CylonicRaider <CylonicRaider@users.noreply.github.com>
2022-02-04 16:36:19 +0100

    Move dynamic data into an instance folder

--- prpn/main.py
+++ prpn/main.py
@@ -8,11 +8,11 @@ import flask
 from . import auth, db, forms, schema
 
 app = flask.Flask('prpn')
-
-DATA_ROOT = os.path.join(app.root_path, '..')
+app.instance_path = os.path.normpath(os.path.join(app.root_path, '..',
+                                                  'data'))
 
 try:
-    KEY_FILE = os.environ.get('KEY_FILE', os.path.join(DATA_ROOT,
+    KEY_FILE = os.environ.get('KEY_FILE', os.path.join(app.instance_path,
                                                        'cookie-key.bin'))
     with open(KEY_FILE, 'rb') as fp:
         app.secret_key = fp.read()
@@ -26,23 +26,24 @@ app.jinja_env.globals.update(
 )
 
 _database = db.LockedDatabase(
-    os.environ.get('DATABASE', os.path.join(DATA_ROOT, 'db.sqlite')),
+    os.environ.get('DATABASE', os.path.join(app.instance_path, 'db.sqlite')),
     schema.init_schema
 )
 get_db = _database.register_to(app, flask.g)
 
 _auth_manager = auth.AuthManager(_database, ())
 
-@app.cli.command('init-key')
-def init_cookie_key():
+@app.cli.command('init')
+def init_files():
+    os.makedirs(os.path.normpath(os.path.join(KEY_FILE, '..')), exist_ok=True)
+    os.makedirs(os.path.normpath(os.path.join(_database.path, '..')),
+                exist_ok=True)
     with open(KEY_FILE, 'ab+') as fp:
         stats = os.fstat(fp.fileno())
-        if stats.st_size != 0:
-            print('Key file already exists')
-            return
-        fp.write(os.urandom(32))
-        os.fchmod(fp.fileno(), 0o600)
-    print('OK (key file created)')
+        if stats.st_size == 0:
+            fp.write(os.urandom(32))
+            os.fchmod(fp.fileno(), 0o600)
+    print('OK')
 
 @app.route('/')
 def index():
