314da675b129d805a4a283005c362e9aabcbab57
CylonicRaider <CylonicRaider@users.noreply.github.com>
2022-02-04 22:54:37 +0100

    Rename main WSGI application

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: ascii -*-
 
-from prpn import app
+from prpn import app as application
 
-if __name__ == '__main__': app.run()
+if __name__ == '__main__': application.run()
