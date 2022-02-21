9046dac6341f47630fb15ae75042368d2f5a077e
CylonicRaider <CylonicRaider@users.noreply.github.com>
2022-02-21 23:48:38 +0100

    Add Scheduler task registry

--- prpn/scheduler.py
+++ prpn/scheduler.py
@@ -3,14 +3,17 @@ import time
 import json
 import threading
 
-IDLE_PAUSE = 300
+IDLE_PAUSE     =   300
+UNKNOWN_RETRY  =   300
+UNKNOWN_EXPIRE = 86400
 
 def init_schema(curs):
     curs.execute('CREATE TABLE IF NOT EXISTS scheduled ('
                      'id INTEGER PRIMARY KEY, '
                      'type TEXT NOT NULL, '
                      'params TEXT NOT NULL, '
-                     'nextRun REAL NOT NULL'
+                     'nextRun REAL NOT NULL, '
+                     'gcAt REAL NOT NULL'
                  ')')
     curs.execute('CREATE INDEX IF NOT EXISTS scheduled_type '
                      'ON scheduled(type)')
@@ -18,8 +21,10 @@ def init_schema(curs):
                      'ON scheduled(nextRun)')
 
 class Scheduler:
-    def __init__(self, db):
+    def __init__(self, db, logger):
         self.db = db
+        self.logger = logger
+        self.commands = {}
         self._cond = threading.Condition()
         self._busy = False
         self._thread = None
@@ -43,6 +48,9 @@ class Scheduler:
         if thr is not None:
             thr.join()
 
+    def register(self, cmd, callback):
+        self.commands[cmd] = callback
+
     def schedule(self, cmd, params, timestamp=None, serial=False):
         if timestamp is None: timestamp = time.time()
         with self.db.transaction(True):
@@ -51,14 +59,33 @@ class Scheduler:
                                            'WHERE type = ? LIMIT 1',
                                        (cmd,))
                 if queued: return
-            self.db.insert('INSERT INTO scheduled(type, params, nextRun) '
-                               'VALUES (?, ?, ?)',
-                           (cmd, json.dumps(params), timestamp))
+            self.db.insert('INSERT INTO scheduled(type, params, nextRun, '
+                                                 'gcAt) '
+                               'VALUES (?, ?, ?, ?)',
+                           (cmd, json.dumps(params), timestamp,
+                            timestamp + UNKNOWN_EXPIRE))
         with self._cond:
             self._cond.notify_all()
 
-    def run_scheduled(self, cmd, params, timestamp):
-        raise NotImplementedError
+    def run_scheduled(self, cmd, params, timestamp, expires):
+        try:
+            callback = self.commands[cmd]
+        except KeyError:
+            if timestamp >= expires:
+                self.logger.error('Unrecognized scheduled task type {!r}; '
+                    'abandoning'.format(cmd))
+                return (False, None)
+            else:
+                self.logger.warning('Unrecognized scheduled task type {!r}; '
+                    'will retry in {}s'.format(cmd, UNKNOWN_RETRY))
+                return (False, timestamp + UNKNOWN_RETRY)
+        try:
+            next_run = callback(cmd, params, timestamp)
+        except BaseException:
+            self.logger.exception('Error while running scheduled task {!r}; '
+                'abandoning'.format(cmd))
+            return (False, None)
+        return (True, next_run)
 
     def _next_task(self, now):
         row = self.db.query('SELECT * FROM scheduled ORDER BY nextRun '
@@ -84,12 +111,18 @@ class Scheduler:
                 row, wait_until = self._next_task()
                 if row is None:
                     continue
-                next_run = self.run_scheduled(row['type'],
-                                              json.loads(row['params']),
-                                              row['nextRun'])
+                params = json.loads(row['params'])
+                ok, next_run = self.run_scheduled(row['type'], params,
+                                                  row['nextRun'], row['gcAt'])
                 if next_run is None:
                     self.db.update('DELETE FROM scheduled WHERE id = ?',
                                    (row['id'],))
+                elif ok:
+                    self.db.update('UPDATE scheduled SET nextRun = ?, '
+                                                        'gcAt = ? '
+                                       'WHERE id = ?',
+                                   (next_run, next_run + UNKNOWN_EXPIRE,
+                                    row['id']))
                 else:
                     self.db.update('UPDATE scheduled SET nextRun = ? '
                                        'WHERE id = ?',
