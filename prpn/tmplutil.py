d4df410afb3d916e7b80682a735a1f7ad4c6a835
CylonicRaider <CylonicRaider@users.noreply.github.com>
2022-07-22 20:12:13 +0200

    Fix sort control edge case

--- prpn/tmplutil.py
+++ prpn/tmplutil.py
@@ -79,19 +79,20 @@ def render_pagination(offset, page_size, cur_page_size, offset_var='offset'):
     result.append(Markup('</ul>'))
     return Markup('').join(result)
 
-def render_sortctl(keyword, query_var='sort'):
+def render_sortctl(keyword, default=None, query_var='sort'):
     cur_value = flask.request.args.get(query_var)
     result = []
-    if cur_value in (keyword, None):
+    if cur_value == keyword or cur_value is None and default == keyword:
         result.append(Markup('<b>\u2193</b>'))
     else:
         result.append(Markup('<a href="%s">\u2193</a>') %
                       add_query_ex({query_var: keyword}))
-    if cur_value == '-' + keyword:
+    neg_kw = '-' + keyword
+    if cur_value == neg_kw or cur_value is None and default == neg_kw:
         result.append(Markup('<b>\u2191</b>'))
     else:
         result.append(Markup('<a href="%s">\u2191</a>') %
-                      add_query_ex({query_var: '-' + keyword}))
+                      add_query_ex({query_var: neg_kw}))
     return Markup('').join(result)
 
 def render_form(title, action, fields, method=Ellipsis, enctype=Ellipsis):
